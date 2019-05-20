6a46bc3b656baceae8777ce411dd7feb84089429
Daniele Zannotti <d.zannotti@me.com>
2019-05-20 01:46:24 +0100

    Implemented better HPF (#35)
    * round one of optimizations
    
    * more math optimisations
    
    * few more precision tweaking
    
    * fixed button colors
    
    * improved HPF

--- src/junox/hpf.js
+++ src/junox/hpf.js
@@ -1,28 +1,31 @@
-import { SQRT2, TWOPI, fastTrig } from './utils'
+// Loosely based on Pirkle's HPF for KG25
+import { TWOPI, fastTanh } from './utils'
 
-export default class Filter {
-  // resonance should be between 1 and 5
-  constructor({ cutoff, resonance, sampleRate }) {
-    this.cutoff = cutoff
-    this.resonance = resonance
-    this.sampleRate = sampleRate
-    this.x = 0
-    this.y = 0
-    this.calculateCoeffients()
+export default class HPF {
+  constructor({ cutoff, sampleRate }) {
+    this.alpha = 1
+    this.z1 = 0
+    this.beta = 1
+    this.T = 1 / sampleRate
+    this.twoT = 2 / this.T
+    this.setCutoff(cutoff)
   }
 
-  calculateCoeffients() {
-    this.z = fastTrig.cos((TWOPI * this.cutoff) / this.sampleRate)
-    this.c = 2 - 2 * this.z
-    const z1 = this.z - 1
-    const resonance = this.resonance * z1
-    this.r = (SQRT2 * Math.sqrt(-Math.pow(z1, 3.0)) + resonance) / resonance
+  setCutoff(cutoff) {
+    const wd = TWOPI * cutoff
+    const wa = this.twoT * Math.tan((wd * this.T) / 2)
+    const g = (wa * this.T) / 2
+    this.alpha = g / (1.0 + g)
   }
 
-  render(input) {
-    this.x = this.x + (input - this.y) * this.c
-    this.y = this.y + this.x
-    this.x = this.x * this.r
-    return input - this.y
+  render(xn) {
+    // calculate v(n)
+    const vn = (xn - this.z1) * this.alpha
+    // form LP output
+    const lpf = vn + this.z1
+    // update memory
+    this.z1 = vn + lpf
+    // do the HPF
+    return fastTanh(xn - lpf)
   }
 }
