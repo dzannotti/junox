6cb135833305f0e93a643d1a706eed6fdf357f52
Daniele Zannotti <d.zannotti@me.com>
2019-05-20 02:40:54 +0100

    typo

--- src/ui/App/elements.js
+++ src/ui/App/elements.js
@@ -11,7 +11,6 @@ export const Container = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  flex-direction: row;
   justify-content: center;
   min-height: 100%;
   min-width: 100%;
