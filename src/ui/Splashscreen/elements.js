8e714a4401069357ebecb51216ef89d3e0385dc1
Daniele Zannotti <d.zannotti@me.com>
2019-05-22 02:35:13 +0100

    typo

--- src/ui/Splashscreen/elements.js
+++ src/ui/Splashscreen/elements.js
@@ -4,7 +4,6 @@ export const Container = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  flex-direction: row;
   justify-content: center;
   min-height: 100%;
   min-width: 100%;
